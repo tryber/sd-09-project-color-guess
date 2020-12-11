14fae5f27e1749e5e7725ac3365e4c179ecd279c
Johne Alves <johne_af@hotmail.com>
2020-12-11 11:12:24 -0300

    adiciona requisito 5

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-const balls = document.getElementsByClassName('ball')
+const rgbColorText = document.getElementById('rgb-color');
+const balls = document.getElementsByClassName('ball');
+const answer = document.getElementById('answer');
+const ballsList = document.getElementById('balls-list')
 
 function ramdomRGB() {
-  const ramdomColor = Math.ceil(Math.random() * 255);
+  const ramdomColor = Math.floor(Math.random() * 256);
   return ramdomColor;
 }
 
 function generateRandomColor() {
   for (let index = 0; index < balls.length; index += 1) {
-    balls[index].style.background = `rgb(${ramdomRGB()}, ${ramdomRGB()}, ${ramdomRGB()})`
+    balls[index].style.background = `rgb(${ramdomRGB()}, ${ramdomRGB()}, ${ramdomRGB()})`;
   }
 }
 
+function colorToGuess() {
+  const colorSelected = balls[Math.floor(Math.random() * 6)];
+  rgbColorText.innerHTML = colorSelected.style.background;
+}
+
+function checkAnswer() {
+  ballsList.addEventListener('click', function (event) {
+    if (event.target.style.background === rgbColorText.innerText) {
+      answer.innerHTML = 'Acertou!';
+    } else {
+      answer.innerHTML = 'Errou! Tente novamente!';
+    }
+  });
+}
+
+// function compareAnswer() {
+//   if (target.style.background === colorSelected.style.background) {
+//     answer.innerHTML = 'Acertou!';
+//   } else {
+//     answer.innerHTML = 'Errou! Tente novamente!';
+//   }
+// }
+
+
 //chamadas
-ramdomRGB();
 generateRandomColor();
+colorToGuess();
+checkAnswer();
