b40c66afcf023b949727f53dde7cea55a5d6e118
Johne Alves <johne_af@hotmail.com>
2020-12-11 11:57:34 -0300

    adiciona requisito bonus

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const rgbColorText = document.getElementById('rgb-color');
 const balls = document.getElementsByClassName('ball');
 const answer = document.getElementById('answer');
 const ballsList = document.getElementById('balls-list');
-const btnReset = document.getElementById('reset-game')
+const btnReset = document.getElementById('reset-game');
+const score = document.getElementById('score');
+let totalScore = parseFloat(localStorage.getItem('Pontuação jogo descubra a cor JAF'));
+score.innerHTML = totalScore
 
 function ramdomRGB() {
   const ramdomColor = Math.floor(Math.random() * 256);
@@ -24,6 +27,9 @@ function checkAnswer() {
   ballsList.addEventListener('click', function (event) {
     if (event.target.style.background === rgbColorText.innerText) {
       answer.innerHTML = 'Acertou!';
+      totalScore += 3
+      localStorage.setItem('Pontuação jogo descubra a cor JAF', totalScore)
+      score.innerHTML = totalScore
     } else {
       answer.innerHTML = 'Errou! Tente novamente!';
     }
@@ -32,13 +38,16 @@ function checkAnswer() {
 
 function resetReloadPage() {
   btnReset.addEventListener('click', function () {
-    window.location.reload()
+    window.location.reload();
   });
 }
-
-//chamadas
-  generateRandomColor();
-  colorToGuess();
-  checkAnswer();
-  resetReloadPage();
-
+// function scoreGame() {
+//   const savedScore = parseFloat(localStorage.getItem('Pontuação jogo descubra a cor JAF'));
+//   return savedScore;
+// }
+// chamadas
+generateRandomColor();
+colorToGuess();
+checkAnswer();
+resetReloadPage();
+scoreGame();
