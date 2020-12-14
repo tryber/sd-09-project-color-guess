ea8fc6cf91d3a18181c7a114d5830b6ee239c61e
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2020-12-14 11:57:37 -0300

    code climate

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const title = document.querySelector('#rgb-color');
 const ballContainer = document.querySelectorAll('.ball');
 const guesser = document.querySelector('#answer');
 const resetBtn = document.querySelector('#reset-game');
-let score = document.querySelector('#score');
+const score = document.querySelector('#score');
 
 
 function aleatoryNumber() {
-    let number = Math.floor(Math.random() * 256);
+    const number = Math.floor(Math.random() * 256);
     return number;
 }
 aleatoryNumber();
@@ -18,8 +18,8 @@ function colorGenerator() {
 colorGenerator();
 
 function addColor() {
-    for(let i = 0; i < ballContainer.length; i += 1) {
-      let insertColor = colorGenerator()
+    for (let i = 0; i < ballContainer.length; i += 1) {
+      const insertColor = colorGenerator();
       ballContainer[i].style.backgroundColor = insertColor;
     }
 }
@@ -27,7 +27,7 @@ addColor();
 
 
 function rbgTitle() {
-    let aleatorySelect = Math.floor(Math.random() * 5);
+    const aleatorySelect = Math.floor(Math.random() * 5);
      title.innerText = ballContainer[aleatorySelect].style.backgroundColor;  
 }
 rbgTitle();
@@ -36,10 +36,10 @@ function guessColor() {
     let counter = 0;
     for (let i = 0; i < ballContainer.length; i += 1) {
         ballContainer[i].addEventListener('click', () => {
-            let colorContainer = ballContainer[i].style.backgroundColor;
-            let titleContent = title.innerText;
+            const colorContainer = ballContainer[i].style.backgroundColor;
+            const titleContent = title.innerText;
 
-            switch(true) {
+            switch (true) {
                 case colorContainer === titleContent:
                     guesser.innerText = 'Acertou!';
                     counter += 3;
@@ -49,16 +49,15 @@ function guessColor() {
                 default:
                     guesser.innerText = 'Errou! Tente novamente!';
                     break;
-     
             }
             launcher();
         })
-    }
+    };
 }
 guessColor();
 
 function resetGame() {
-    resetBtn.addEventListener('click', ()=> {
+    resetBtn.addEventListener('click', () => {
         guesser.innerText = 'Escolha uma cor';
         launcher();
     })
@@ -70,4 +69,3 @@ function launcher() {
     addColor();
     rbgTitle();   
 }
-
