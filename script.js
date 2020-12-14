091474a1d2c6dc49c4d75aa7ef806b113b5352b2
Giovani L D <giovani.diccini@hotmail.com>
2020-12-14 17:44:23 -0300

    more functions

--- script.js
+++ script.js
@@ -8,8 +8,47 @@ window.onload = function(){
         colorsDiv.appendChild(divElement);
     };
 
-    // Injetando rgb aleatório no parágrafo
+    randomColor();
+}
+
+// Injetando rgb aleatório no parágrafo
+function randomColor(){
     let colorsList = document.querySelectorAll('.ball');
     let rgbColor = document.getElementById('rgb-color');
     rgbColor.innerText = colorsList[Math.floor(Math.random() * 6)].style.backgroundColor;
 }
+
+
+let points = 0;
+
+// Função jogar
+function play(){
+    let ballClick = document.querySelector('#colors');
+    ballClick.addEventListener('click', function(event){
+        let eventRgb = event.target.style.backgroundColor;
+        let answer = document.querySelector('#rgb-color').innerText;
+        if (eventRgb === answer){
+            document.querySelector('#right-wrong').innerText = 'Acertou!';
+            points += 3;
+            document.querySelector('#score').innerText = `Pontos: ${points}`;
+        } else {
+            document.querySelector('#right-wrong').innerText = 'Errou! Tente novamente!';
+        }
+    });
+};
+
+play();
+
+// Função resetar jogo
+function resetGame(){
+    let resetBtn = document.getElementById('reset');
+    resetBtn.addEventListener('click', function(){
+        let listOfColors = document.querySelectorAll('.ball');
+        for(let index = 0; index < 6; index += 1){
+            listOfColors[index].style.backgroundColor = `rgb(${Math.ceil(Math.random() * 255)}, ${Math.ceil(Math.random() * 255)}, ${Math.ceil(Math.random() * 255)})`;
+        }
+        randomColor();
+    });
+};
+
+resetGame();
