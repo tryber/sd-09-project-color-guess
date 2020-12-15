d9c87a2e8d77f00a5974b12f1da1381f7b4a9bbd
Tiago Yoneda <xyonedax@gmail.com>
2020-12-15 18:26:53 -0300

    arrumando para codeClimate

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 // Só para ajudar a mexer com as cores
-function hexToRbg (hexcode){
+function calculateColor(hex1, hex2) {
+  hex1 = hex1*16
+  return (hex1 + hex2);
+}
+
+function hexToRbg(hexcode) {
   hexcode = hexcode.toUpperCase();
   const correspondence = {
-    0 : 0,
-    1 : 1,
-    2 : 2,
-    3 : 3,
-    4 : 4,
-    5 : 5,
-    6 : 6,
-    7 : 7,
-    8 : 8,
-    9 : 9,
-    A : 10,
-    B : 11,
-    C : 12,
-    D : 13,
-    E : 14,
-    F : 15,
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    A: 10,
+    B: 11,
+    C: 12,
+    D: 13,
+    E: 14,
+    F: 15,
   }
-let rComponent = (correspondence[hexcode[0]]*16 + correspondence[hexcode[1]]);
-let gComponent = (correspondence[hexcode[2]]*16 + correspondence[hexcode[3]]);;
-let bComponent = (correspondence[hexcode[4]]*16 + correspondence[hexcode[5]]);;
+  const rComponent = calculateColor(correspondence[hexcode[0]], correspondence[hexcode[1]]);
+  const gComponent = calculateColor(correspondence[hexcode[2]], correspondence[hexcode[3]]);
+  const bComponent = calculateColor(correspondence[hexcode[4]], correspondence[hexcode[5]]);
 
-return `rgb(${rComponent} , ${gComponent} , ${bComponent})`;
+  return `rgb(${rComponent}, ${gComponent}, ${bComponent})`;
 }
 
 const myColorExchange = '457b9d';
@@ -39,16 +44,16 @@ function generateRandomColor() {
 }
 
 function pickRandom(array) {
-  let randElement = Math.floor(Math.random() * array.length);
+  const randElement = Math.floor(Math.random() * array.length);
   return randElement;
 }
 
-function generateBalls() { 
-  let colorArray = [];
-  for ( let index = 0; index < 6; index += 1){
+function generateBalls() {
+  const colorArray = [];
+  for (let index = 0; index < 6; index += 1) {
     const myColor = generateRandomColor();
     colorArray.push(myColor);
-    
+
     const myColorBall = document.createElement('div');
     myColorBall.className = 'ball';
     myColorBall.style.backgroundColor = `rgb${myColor}`;
