cf85da1de8b06c9ffc094748f305619712d5bcbc
Qu4k3r <neves0007@gmail.com>
2020-12-15 09:26:25 -0300

    Iniciando projeto bonus xuxu beleza adivinhe a cor :rocket:

---
+++ script.js
@@ -0,0 +1,22 @@
+/* Requisito 2 - mostrar o texto rgb a ser adivinhado;
+   id = rgb-color;
+   formato (168, 34, 1) => exemplo;
+*/
+
+function randomNumber() {
+  let randomNumber = parseInt(Math.random() * 1000);
+  while (randomNumber > 255) {
+    randomNumber = parseInt(Math.random() * 1000);
+    if (randomNumber <= 255) {
+      break
+    }
+  }
+  return randomNumber;
+}
+
+function setRandomRgb() {
+  const randomRgb = document.querySelector('#rgb-color');
+  randomRgb.innerText = `(${randomNumber()} ,${randomNumber()} ,${randomNumber()})`
+}
+
+setRandomRgb();
