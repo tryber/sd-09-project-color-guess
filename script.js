fbc4666c1e081c1022566a8fee41049afd2d1683
danielfasanaro <danielfasanaro@gmail.com>
2020-12-16 14:01:16 -0300

    5th Requiriment - Added colorGuesing functon and some other elements interactions

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const rgbColor = document.querySelector('#rgb-color');
 const balls = document.querySelectorAll('.ball');
+const answer = document.querySelector('#answer');
+const answerOptions = document.querySelector('#answer-options');
 
 function randomRGB() {
   const red = Math.floor(Math.random() * 255);
@@ -7,10 +10,34 @@ function randomRGB() {
   return (`rgb(${red}, ${green}, ${blue})`);
 }
 
-function coloringCircles() {
+function setCorrectAnswer() {
+  const correctAnswer = balls[Math.floor(Math.random() * 6)].style.backgroundColor.slice(3);
+  rgbColor.innerText = correctAnswer;
+}
+
+function setCircleColors() {
   for (let index = 0; index < balls.length; index += 1) {
     balls[index].style.backgroundColor = randomRGB();
   }
-  console.log('coloringCircles');
 }
-coloringCircles();
+
+function colorGuessing(event) {
+  const playerAnswer = event.target.style.backgroundColor.slice(3);
+  let answerText = 'Errou! Tente novamente!';
+  if (playerAnswer === rgbColor.innerText) {
+    answerText = 'Acertou!';
+  }
+  answer.innerText = answerText;
+}
+
+function addEventListeners() {
+  for (let index = 0; index < balls.length; index += 1) {
+    balls[index].addEventListener('click', colorGuessing);
+  }
+}
+
+{
+  setCircleColors();
+  setCorrectAnswer();
+  addEventListeners();
+}
