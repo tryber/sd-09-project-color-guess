7d2d1b620b777fb37fa3c43b444e00d2601d5aff
rach-vp <picanco.raquel@gmail.com>
2020-12-16 22:03:26 -0400

    Cria as opções a serem selecionadas pelo usuário

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 function randomColor() {
-  // Arredondar para baixo para não cair no branco
-  const rComponent = Math.floor(Math.random() * 255);
-  const gComponent = Math.floor(Math.random() * 255);
-  const bComponent = Math.floor(Math.random() * 255);
+  const rComponent = Math.round(Math.random() * 255);
+  const gComponent = Math.round(Math.random() * 255);
+  const bComponent = Math.round(Math.random() * 255);
   return `(${rComponent}, ${gComponent}, ${bComponent})`;
 }
 
+/*
+Método para embralhar array descrito no vídeo abaixo:
+
+https://www.youtube.com/watch?v=5sNGqsMpW1E&feature=emb_logo
+*/
+function shuffle(array) {
+  for (let index = array.length - 1; index > 0; index -= 1) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    const temp = array[index];
+    array[index] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
+  return array;
+}
+
+function setOptions(secretColor) {
+  const balls = document.querySelectorAll('.ball');
+  let ballsColors = [secretColor];
+  for (let index = 1; index < balls.length; index += 1) {
+    ballsColors.push(randomColor());
+  }
+  ballsColors = shuffle(ballsColors);
+  for (let index = 0; index < balls.length; index += 1) {
+    balls[index].style.backgroundColor = `rgb${ballsColors[index]}`;
+  }
+}
+
 window.onload = function () {
+  // Cria rgb da cor secreta
   const mainContent = document.querySelector('.main-content');
   const rgbColor = document.createElement('p');
   rgbColor.id = 'rgb-color';
   rgbColor.innerText = randomColor();
-  mainContent.appendChild(rgbColor);
+  mainContent.prepend(rgbColor);
+
+  // Popula opções
+  setOptions(rgbColor.innerText);
 };
