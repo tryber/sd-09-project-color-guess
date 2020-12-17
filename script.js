51164e9317a30c1f9b23d9d98d47fb5f86fc0346
Henrique Brito Elias <henriquebelias@gmail.com>
2020-12-17 15:13:27 -0300

    -Requisito 7-

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const answer = document.querySelector('#answer');
 const ballContainer = document.querySelector('#ball-container');
 const resetBt = document.querySelector('#reset-game');
 const score = document.querySelector('#score');
+let pontuacao = 0;
 
 window.onload = function () {
-  score.innerText = 0;
   if (sessionStorage.score === undefined) {
-    score.innerText = 0;
+    score.innerHTML = 0;
   } else {
-    score.innerText = sessionStorage.score;
+    pontuacao = parseInt(sessionStorage.score, 10);
+    score.innerHTML = pontuacao;
   }
 };
 
@@ -30,25 +31,19 @@ coloredBalls();
 
 function colorValue() {
   const ballsIndex = Math.floor(Math.random() * balls.length);
-  rgbColor.innerText = balls[ballsIndex].style.backgroundColor;
+  rgbColor.innerHTML = balls[ballsIndex].style.backgroundColor;
 }
 colorValue();
 
 function pickTheColor() {
   ballContainer.addEventListener('click', function (event) {
-    if (event.target.style.backgroundColor === rgbColor.innerText) {
-      answer.innerText = 'Acertou!';
-      if (sessionStorage.score !== undefined) {
-        let pontuacao = parseInt(sessionStorage.score, 10);
-        pontuacao += 3;
-        sessionStorage.score = pontuacao;
-        score.innerText = pontuacao;
-      } else {
-        sessionStorage.score = 3;
-        score.innerText = sessionStorage.score;
-      }
+    if (event.target.style.backgroundColor === rgbColor.innerHTML) {
+      answer.innerHTML = 'Acertou!';
+      pontuacao += 3;
+      sessionStorage.score = pontuacao;
+      score.innerHTML = pontuacao;
     } else {
-      answer.innerText = 'Errou! Tente novamente!';
+      answer.innerHTML = 'Errou! Tente novamente!';
     }
   });
 }
