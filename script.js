93399f10b8f4a822429ae70feb96197232083c46
IvanRafael <ivanrafael.dev@gmail.com>
2020-12-18 02:19:30 -0300

    requisito 3

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const corDaVez = 'rgb' + randomRGB();
-
+const valoresRGB = randomRGB();
+const rgbDaVez = `rgb${randomRGB()}`;
 function addTitle() {
   const header = document.querySelector("header");
   const title = document.createElement("h1");
   title.id = "title";
-  title.style.color = corDaVez;
+  title.style.color = rgbDaVez;
   title.innerText = "Color Guess!!";
   header.appendChild(title);
 }
@@ -13,8 +13,8 @@ function addRBG() {
   const main = document.querySelector("main");  
   const title = document.createElement("h2");
   title.id = "rgb-color";
-  title.style.color = corDaVez;
-  title.innerText = randomRGB();
+  title.style.color = rgbDaVez;
+  title.innerText = valoresRGB;
   main.appendChild(title);
 }
 
@@ -25,7 +25,32 @@ function randomRGB() {
   return `(${num1}, ${num2}, ${num3})`
 }
 
+// function randomMatchColorCircle(){
+let bolaDaVez = Math.round(Math.random() * 6)  
+// }
+
+function addCircles() {
+  const div = document.querySelector('div');
+  for (let index = 0; index < 6; index += 1) {   
+    const circle = document.createElement('div');
+    circle.classList.add('ball');
+    circle.style.backgroundColor = 'rgb' + randomRGB();
+    circle.style.display = 'inline-block';
+    circle.style.width = '100px';
+    circle.style.height = '100px';
+    circle.style.borderRadius = '50%';
+    circle.style.border = '1px solid black';
+    circle.style.marginRight = '10px';
+    if (index == bolaDaVez){
+    alert('a resposta é ' + (index+1))
+    circle.style.backgroundColor = rgbDaVez;
+    }
+    div.appendChild(circle);
+  }
+}
+
 window.onload = function () {
   addTitle();
   addRBG();
+  addCircles();
 };
