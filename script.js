7228e395e9207490cedd3fc2af161831d700cf33
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-18 15:38:32 -0300

    texto com cor rgb para ok

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function createDivsBall(n) {
   }
 }
 
-
 function colorRandom(n) {
   const rgbColorlist = [];
   for (let i = 0; i < n; i += 1) {
@@ -20,25 +19,33 @@ function colorRandom(n) {
   return rgbColorlist;
 }
 
-
 function colorBalls() {
   const arrayBalls = document.querySelectorAll('.ball');
   const rgbColorlist = colorRandom(arrayBalls.length);
   for (let i = 0; i < arrayBalls.length; i += 1) {
     arrayBalls[i].style.backgroundColor = `rgb(${rgbColorlist[i]})`;
   }
+  return rgbColorlist;
 }
 
 function reset() {
   colorBalls();
+  colorToFind()
 }
 
 function createBalls(amount) {
-  createDivsBall(amount)
+  createDivsBall(amount);
   colorRandom(amount);
   colorBalls();
+  colorToFind()
+
 }
 
+function colorToFind() {
+  const textColorToFind = document.querySelector('#rgb-color');
+  const colorList = colorBalls();
+  textColorToFind.innerHTML =  `(${colorList[Math.floor(Math.random() * 6 )]})`;
+}
 createBalls(6);
 
 const btnReset = document.querySelector('#reset-game');
