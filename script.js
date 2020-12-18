1f92c0194670780571c22bce493f11abe3508056
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-18 08:20:14 -0300

    Function colorRandom concluida e function colorBalls em desenvolvimento

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ function createBalls(n) {
     divContainerBall.appendChild(divBall);
   }
 }
+
+createBalls(6)
+
+function colorRandom(n) {
+  const rgbColorlist = [];
+  for (let i = 0; i < n; i += 1) {
+    const rgbColor = [];
+    for ( let j = 0; j < 3; j += 1) {
+      rgbColor.push(Math.floor(Math.random() * 256));
+    }
+    rgbColorlist.push(rgbColor);
+  }
+  return rgbColorlist;
+}
+
+colorRandom(6);
+
+function colorBalls() {
+  const arrayBalls = document.querySelectorAll('.ball');
+  const rgbColorlist = colorRandom(arrayBalls.length);
+  for (let i = 0; i < arrayBalls.length; i += 1) {
+
+
+
+  }
+}
+
+colorBalls();
