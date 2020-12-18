84475effb1f921104b03a9a3741d8dc48204e1a7
Felipe Cardoso <felipedclc@gmail.com>
2020-12-18 10:45:49 -0300

    requisito 5

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ function getRandomRGB(min, max) {
 }
 
 function createRgbInPage() {
-  const h2 = document.createElement('h2');
+  const p = document.createElement('p');
   // h2.innerText = `(${getRandomRGB(0, 255)}), (${getRandomRGB(0, 255)}), (${getRandomRGB(0, 255)})`;
-  h2.innerText = '(168, 34, 1)';
+  p.innerText = '(168, 34, 1)';
+  p.style.background = 'black';
+  p.style.color = 'white';
+  p.style.padding = '5px';
   const paiH2 = document.querySelector('#rgb-color');
-  paiH2.appendChild(h2);
+  paiH2.appendChild(p);
 }
 createRgbInPage();
 
@@ -22,9 +25,38 @@ function createPalleteColors(color) {
   const divColorPai = document.querySelector('#colors');
   divColorPai.appendChild(divColor);
 }
-createPalleteColors(`rgb(${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)})`);
+
+/* function generateColorsCircles() {
+  const balls = document.querySelectorAll('.ball');
+  for (let i = 0; i <= 4; i += 1) {
+    balls[i] = createPalleteColors(`rgb(${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)})`);
+  }
+  balls == createPalleteColors('rgb(168, 34, 1)');
+}
+generateColorsCircles(); */
+
 createPalleteColors(`rgb(${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)})`);
 createPalleteColors(`rgb(${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)})`);
 createPalleteColors(`rgb(${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)})`);
 createPalleteColors('rgb(168, 34, 1)');
 createPalleteColors(`rgb(${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)})`);
+createPalleteColors(`rgb(${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)})`);
+
+function createAnswer() {
+  const divAnswer = document.querySelector('#answer');
+  const colors = document.querySelector('#colors');
+  const crazyText = document.createElement('p');
+  crazyText.innerText = 'Escolha uma cor';
+  divAnswer.appendChild(crazyText);
+
+  colors.addEventListener('click', function (e) {
+    if (e.target.style.backgroundColor === 'rgb(168, 34, 1)') {
+      crazyText.innerText = 'Acertou!';
+      crazyText.style.color = 'green';
+    } else {
+      crazyText.innerText = 'Errou! Tente novamente!';
+      crazyText.style.color = 'red';
+    }
+  });
+}
+createAnswer();
