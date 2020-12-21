cb57f2b584b11a0128e12ae09874509e31214643
fernandacajueiro <fernandacajueiromachado@gmail.com>
2020-12-21 17:39:09 -0300

    Adiciona requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function randomRgb() {
-  let red = Math.round(Math.random() * 255);
-  let green = Math.round(Math.random() * 255);
-  let blue = Math.round(Math.random() * 255);
+  const red = Math.round(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
@@ -9,6 +9,16 @@ const color = randomRgb();
 const rgbValue = document.getElementById('rgb-color');
 rgbValue.innerText = color.slice(3);
 
-window.onload = function () {
+function createColors() {
+  const section = document.getElementById('colors');
+  for (let index = 0; index < 6; index += 1) {
+    const circle = document.createElement('div');
+    circle.classList.add('ball');
+    circle.style.backgroundColor = randomRgb();
+    section.appendChild(circle);
+  }
+}
 
+window.onload = function () {
+  createColors();
 };
