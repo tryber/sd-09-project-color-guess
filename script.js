9d79a473f6acf6abc263a5a6f0b92d8cac04272f
fernandacajueiro <fernandacajueiromachado@gmail.com>
2020-12-21 18:09:56 -0300

    Adiciona requisito 5

--- script.js
+++ script.js
@@ -5,20 +5,46 @@ function randomRgb() {
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
-const color = randomRgb();
+const chosenColor = randomRgb();
 const rgbValue = document.getElementById('rgb-color');
-rgbValue.innerText = color.slice(3);
+rgbValue.innerText = chosenColor.slice(3);
+const answer = document.getElementById('answer');
+answer.innerText = 'Escolha uma cor';
 
 function createColors() {
   const section = document.getElementById('colors');
   for (let index = 0; index < 6; index += 1) {
-    const circle = document.createElement('div');
-    circle.classList.add('ball');
-    circle.style.backgroundColor = randomRgb();
-    section.appendChild(circle);
+    const circles = document.createElement('div');
+    circles.classList.add('ball');
+    circles.style.backgroundColor = randomRgb();
+    section.appendChild(circles);
   }
+}
+
+function chosenCircle() {
+  const circles = document.getElementsByClassName('ball');
+  const chosen = Math.round(Math.random() * 5);
+  for (let index = 0; index < circles.length; index += 1) {
+    if (index === chosen) {
+      circles[index].style.backgroundColor = chosenColor;
+    }
+  }
+}
+
+function checkAnswer() {
+  const circles = document.getElementById('colors');
+  const answer = document.getElementById('answer');
+  circles.addEventListener('click', function (event) {
+    if (event.target.style.backgroundColor === chosenColor) {
+      answer.innerText = 'Acertou!'
+    } else {
+      answer.innerText = 'Errou! Tente novamente!'
+    }
+  });
 }
 
 window.onload = function () {
   createColors();
+  chosenCircle();
+  checkAnswer();
 };
