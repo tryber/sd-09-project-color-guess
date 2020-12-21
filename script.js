49af0c55f5fe730ece194992349052bc15cbde2a
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-21 04:30:02 -0300

    Pontuaçao a cada acerto

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function createDivsBall(n) {
 function colorRandom(n) {
   const rgbColorlist = [];
   for (let i = 0; i < n; i += 1) {
-    const rgbColor = [];
+    let rgbColor = [];
     for ( let j = 0; j < 3; j += 1) {
       rgbColor.push(Math.floor(Math.random() * 256));
     }
@@ -44,11 +44,38 @@ function createBalls(amount) {
 function colorToFind() {
   const textColorToFind = document.querySelector('#rgb-color');
   const colorList = colorBalls();
-  textColorToFind.innerHTML =  `(${colorList[Math.floor(Math.random() * 6 )]})`;
+  textColorToFind.innerHTML = `(${colorList[Math.floor(Math.random() * 6 )].join(', ')})`;
 }
-createBalls(6);
 
-const btnReset = document.querySelector('#reset-game');
-btnReset.addEventListener('click', reset);
+function checkChoice (event) {
+   let choice = event.target.style.backgroundColor;
 
+  const rgbColor = `rgb${document.getElementById('rgb-color').innerText}`;
 
+  console.log(rgbColor, 'pc');
+  console.log(choice, 'Escolha');
+
+  if (choice === rgbColor) {
+    console.log('Acertou')
+    scorePoints();
+  }
+}
+
+const scorePoints = () => {
+  const scoreTextElement = document.getElementById('score');
+  let scoreText = parseInt(scoreTextElement.innerText);
+  scoreText += 3;
+  scoreTextElement.innerText = scoreText;
+  console.log(scoreText)
+}
+
+window.onload = () => {
+
+  const btnReset = document.querySelector('#reset-game');
+  btnReset.addEventListener('click', reset);
+
+  createBalls(6);
+
+  const arrayBalls = document.querySelector('#conteiner-ball');
+  arrayBalls.addEventListener('click', checkChoice);
+}
