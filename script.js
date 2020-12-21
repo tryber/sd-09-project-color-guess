5e5230c2abf0a95bf7c254b24273670dfd645c9d
Renato Feitosa <pythaqua@gmail.com>
2020-12-20 23:32:26 -0300

    [refac.] 2nd requirement

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 const colorToBeGuessedSpan = document.getElementById('rgb-color');
-const ballsContainerDiv = document.getElementById('balls-container');
-
-function setPropertiesToNewElement(element, propertiesObject) {
-  const propertiesKeys = Object.keys(propertiesObject);
-  for (let index = 0; index < propertiesKeys.length; index += 1) {
-    const key = propertiesKeys[index];
-    element[key] = propertiesObject[key];
-  }
-}
-
-function createNewElement(tag, propertiesObject) {
-  const newElement = document.createElement(tag);
-  setPropertiesToNewElement(newElement, propertiesObject);
-  return newElement;
-}
 
 function getIntensityValue() {
   return Math.round(Math.random() * 255);
@@ -28,8 +13,6 @@ function getRandomRGB() {
 
 const colorToBeGuessedCode = getRandomRGB();
 colorToBeGuessedSpan.innerText = colorToBeGuessedCode;
-
-
 
 window.onload = function () {
 }
