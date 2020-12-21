869c6c4c7f22994e9e877440a05a52b844e1ecdd
Felipe Cardoso <felipedclc@gmail.com>
2020-12-21 16:57:26 -0300

    requisito final / code climate

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function getRandomRGB(min, max) {
 }
 const selectedNumbers = '(168, 34, 1)';
 const selectedRGB = 'rgb(168, 34, 1)';
+const selectedRandomColor = `rgb(${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)})`;
 
 function createRgbInPage() {
   const p = document.createElement('p');
@@ -55,6 +56,16 @@ function createAnswer(rightColor) {
 }
 createAnswer(selectedRGB);
 
+function sortBalls() {
+  // https://stackoverflow.com/questions/25175798/how-to-shuffle-a-nodelist
+  let list = document.querySelector('#colors'),
+    i;
+  for (i = list.children.length; i >= 0; i -= 1) {
+    list.appendChild(list.children[Math.random() * i | 0]);
+  }
+}
+sortBalls();
+
 function createBtnReload() {
   const btn = document.createElement('button');
   btn.style.padding = '5px';
@@ -67,14 +78,12 @@ function createBtnReload() {
     const p = document.querySelector('#correct');
     const paiP = document.querySelector('#rgb-color');
     const balls = document.querySelectorAll('.ball');
-    const selectedRandomColor = `rgb(${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)}, ${getRandomRGB(0, 255)})`;
 
     p.innerText = selectedRandomColor;
     paiP.appendChild(p);
     balls.forEach((e) => e.parentNode.removeChild(e));
     generateColorsCircles();
     createPalleteColors(selectedRandomColor);
-    console.log(selectedRandomColor);
     const divAnswer = document.querySelector('#answer');
     const text = document.querySelector('#text');
 
@@ -85,46 +94,29 @@ function createBtnReload() {
 }
 createBtnReload();
 
-function sortBalls() {
-  /*
-  const colorBalls = document.querySelectorAll('.ball'); // node list
-  const arrayColorBalls = [].slice.call(colorBalls); // array de nodes 
-  */
-
-  // https://stackoverflow.com/questions/25175798/how-to-shuffle-a-nodelist
-  let list = document.querySelector('#colors'),
-    i;
-  for (i = list.children.length; i >= 0; i--) {
-    list.appendChild(list.children[Math.random() * i | 0]);
-  }
-}
-sortBalls();
-
 function createScore() {
   const p = document.createElement('p');
   p.id = 'placar';
-  p.innerText = 'Placar: ';
+  p.innerText = 'Pontuação: ';
   const paiP = document.querySelector('#score');
   paiP.appendChild(p);
 
   const scoreSpan = document.createElement('span');
+  scoreSpan.id = 'num';
   const colors = document.querySelector('#colors');
 
   let score = 0;
-  colors.addEventListener('click', function () {
+  scoreSpan.innerText = score;
+  p.appendChild(scoreSpan);
+
+  colors.addEventListener('click', function (e) {
+    e.preventDefault();
     const answer = document.querySelector('#answer');
     const text = document.querySelector('#text');
-    if (text.innerText == 'Acertou!') {
-      score += 1;
+    if (text.innerText === 'Acertou!') {
+      score += 3;
       scoreSpan.innerText = score;
       p.appendChild(scoreSpan);
-    }
-    if (score > 2) {
-      answer.removeChild(text);
-      text.innerText = 'Parabéns, você acaba de ganhar uma viagem para Prados city!';
-      text.style.backgroundColor = 'yellow';
-      text.style.color = 'blue';
-      answer.appendChild(text);
     }
   });
 }
