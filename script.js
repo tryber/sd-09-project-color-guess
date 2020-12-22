377c35fd9cd5ec71c2fea5a6388ff0cc6e14e80d
Pamela Anderson Sales <pamelasalesr@gmail.com>
2020-12-22 12:47:25 -0300

    Add requirements 1 and 2

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+  createColorToBeGuessed();
+}
+
+function randomColorNumbers() {
+  return Math.round(Math.random() * 255);
+}
+
+function createColorToBeGuessed() {
+  const rgbNumbers = document.getElementById('rgb-color');
+  rgbNumbers.innerText = `(${randomColorNumbers()}, ${randomColorNumbers()}, ${randomColorNumbers()})`;
+}
