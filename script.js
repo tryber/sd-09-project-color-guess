9e3bf8acc43eb959a929ed1b568b62a4a21ed7f0
Osvaldo Borges <netoo70@gmail.com>
2020-12-22 14:59:16 -0300

    Requisito 03 e 04

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 function randomColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let index = 0; index < 6; index += 1) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let index = 0; index < 6; index += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
-const hexToRgb = hex =>
-  hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
-             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
-    .substring(1).match(/.{2}/g)
-    .map(x => parseInt(x, 16))
+function createBall(idName) {
+  const father = document.querySelector('.ball-section')
+  const ballCreate = document.createElement('div')
+  ballCreate.id = idName;
+  ballCreate.className = 'ball';
+  father.appendChild(ballCreate);
+}
+
+function numberBall(number) {
+  for (index = 1; index <= number; index += 1) {
+    createBall(`ball-${index}`);
+  }
+}
+numberBall(6);
+
+function randomColorBall() {
+  const ball = document.querySelectorAll('.ball');
+  for (index = 0; index < ball.length; index += 1) {
+    ball[index].style.backgroundColor = randomColor();
+  }
+}
+randomColorBall();
+
+//Referencia https://convertingcolors.com/blog/article/convert_hex_to_rgb_with_javascript.html
+let aRgbHex = randomColor().match(/.{1,2}/g);
+let aRgb = [
+    parseInt(aRgbHex[0], 16),
+    parseInt(aRgbHex[1], 16),
+    parseInt(aRgbHex[2], 16)
+];
 
 function colorText() {
-    const color = document.querySelector('#rgb-color');
-    color.innerHTML = `(${hexToRgb(randomColor())})`;
+  const color = document.querySelector('#rgb-color');
+  const ball = document.querySelectorAll('.ball');
+  color.innerHTML = ball[Math.floor(Math.random() * 6)].style.backgroundColor.slice(3)
 }
 colorText();
