bae3cce8bbc40a04e532e4b43b4058ade178e384
Pamela Anderson Sales <pamelasalesr@gmail.com>
2020-12-22 14:56:06 -0300

    Add requirement 4

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 function randomColorNumbers() {
-  return Math.round(Math.random() * 255);
+  const red = Math.round(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
+  return `rgb(${red}, ${green}, ${blue})`;
 }
+
+let colorOfTheRound = randomColorNumbers();
+const circles = document.getElementsByClassName('ball');
 
 function createColorToBeGuessed() {
   const rgbNumbers = document.getElementById('rgb-color');
-  rgbNumbers.innerText = `(${randomColorNumbers()}, ${randomColorNumbers()}, ${randomColorNumbers()})`;
+  rgbNumbers.innerText = colorOfTheRound.slice(3);
+}
+
+function createBackgroundCircle() {
+  for (let index = 0; index < 6; index += 1) {
+    circles[index].style.backgroundColor = randomColorNumbers();
+  }
+}
+
+function chosenColor() {
+  const chosenIndex = Math.round(Math.random() * 5);
+  for (let index = 0; index < circles.length; index += 1) {
+    if (index === chosenIndex) {
+      circles[index].style.backgroundColor = colorOfTheRound;
+    } else {
+      circles[index].style.backgroundColor = randomColorNumbers();
+    }
+  }
 }
 
 window.onload = function () {
   createColorToBeGuessed();
+  createBackgroundCircle();
+  chosenColor();
 }
