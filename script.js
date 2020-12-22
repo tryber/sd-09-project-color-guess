116a903117723a985b99ce0a9b9eea4e82095e67
fernandacajueiro <fernandacajueiromachado@gmail.com>
2020-12-22 16:16:57 -0300

    Tenta de novo ver o erro do Code Climate

--- script.js
+++ script.js
@@ -67,13 +67,13 @@ function clickResetGame() {
   });
 }
 
-/*function clickResetScore() {
+/* function clickResetScore() {
   const button = document.getElementById('reset-score');
   button.addEventListener('click', function () {
     result = 0;
     score.innerText = result;
   });
-}*/
+} */
 
 window.onload = function () {
   createColors();
