62c7d9d5ce92184e92717111ce960795bc6d7e90
marcusWittho <witthoeft.mcs@hotmail.com>
2020-12-23 17:35:56 -0300

    Color Guess requisito 7

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const balls = document.querySelectorAll('.ball');
 const colection = document.querySelector('.colection');
 const answer = document.querySelector('#answer');
 const resetGame = document.querySelector('#reset-game');
+const score = document.querySelector('#score');
 const colors = [];
+let repeatedClick = true;
+let scoreboard = 0;
 
 function randomNumber(multiply) {
   const number = Math.floor(Math.random() * multiply);
@@ -22,6 +25,7 @@ function toColor() {
   for (let index = 0; index < balls.length; index += 1) {
     balls[index].style.backgroundColor = randomRgb();
     colors[index] = balls[index].style.backgroundColor;
+    balls[index].addEventListener('click', changeScoreboard);
   }
 }
 
@@ -31,13 +35,28 @@ function reference() {
 
 colection.addEventListener('click', (event) => {
   const chosenColor = event.target.style.backgroundColor;
-  (chosenColor === rgbColor.innerText) ? answer.innerText = 'Acertou!' : answer.innerText = 'Errou! Tente novamente!';
+
+  if (chosenColor === rgbColor.innerText) {
+    answer.innerText = 'Acertou!';
+  } else {
+    answer.innerText = 'Errou! Tente novamente!';
+  }
 });
+
+function changeScoreboard(event) {
+  let selectedColor = event.target.style.backgroundColor;
+  if (selectedColor === rgbColor.innerText && repeatedClick === true) {
+    scoreboard += 3;
+    repeatedClick = false;
+  }
+  score.innerText = scoreboard;
+}
 
 resetGame.addEventListener('click', () => {
   toColor();
   reference();
   answer.innerText = 'Escolha uma cor';
+  repeatedClick = true;
 });
 
 window.onload = function () {
