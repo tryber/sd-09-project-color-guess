6816cf914784045804057f8b4e264cfaab071060
Rafael Azevedo <rafaelazevedo321@gmail.com>
2020-12-26 17:57:47 -0300

    feito requisito 4

--- script.js
+++ script.js
@@ -17,7 +17,16 @@ function generateRgbColor() {
   return rgbColor;
 }
 
+function colectionColors() {
+  const listColors = document.querySelectorAll('.ball');
+  console.log(listColors);
+  for (let index = 0; index < listColors.length; index += 1) {
+    listColors[index].style.backgroundColor = 'rgb' + generateRgbColor();
+  }
+}
+
 window.onload = function() {
   const rgb = document.querySelector('#rgb-color');
   rgb.innerText = generateRgbColor();
+  colectionColors();
 };
