8f8292c45c7067085fb209c2fdb4ec7c195eb57f
Rafael Azevedo <rafaelazevedo321@gmail.com>
2020-12-26 17:40:10 -0300

    corrigido codeclimate

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function generateRgbColor() {
-    let number;
-    let rgbColor = '';
-    for (let index = 1; index <= 3; index += 1) {
-        number = Math.floor(Math.random() * 256);
-        switch (index) {
-            case 1:
-                rgbColor = '(' + number;
-                break;
-            case 2:
-                rgbColor += ', ' + number;
-                break;
-            case 3:
-                rgbColor += ', ' + number + ')';
+  let number;
+  let rgbColor = '';
+  for (let index = 1; index <= 3; index += 1) {
+    number = Math.floor(Math.random() * 256);
+      switch (index) {
+        case 1:
+          rgbColor = '(' + number;
+          break;
+        case 2:
+          rgbColor += ', ' + number;
+          break;
+        case 3:
+          rgbColor += ', ' + number + ')';
         }
-    }
-    return rgbColor;
+  }
+  return rgbColor;
 }
 
 window.onload = function() {
-    const rgb = document.querySelector('#rgb-color');
-    rgb.innerText = generateRgbColor();
+  const rgb = document.querySelector('#rgb-color');
+  rgb.innerText = generateRgbColor();
 };
