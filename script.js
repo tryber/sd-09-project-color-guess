9382572f5bf362bf4e5a6749871e71423bea8498
Rafael Azevedo <rafaelazevedo321@gmail.com>
2020-12-26 17:29:18 -0300

    feito requisito 2

---
+++ script.js
@@ -0,0 +1,23 @@
+function generateRgbColor() {
+    let number;
+    let rgbColor = '';
+    for (let index = 1; index <= 3; index += 1) {
+        number = Math.floor(Math.random() * 256);
+        switch (index) {
+            case 1:
+                rgbColor = '(' + number;
+                break;
+            case 2:
+                rgbColor += ', ' + number;
+                break;
+            case 3:
+                rgbColor += ', ' + number + ')';
+        }
+    }
+    return rgbColor;
+}
+
+window.onload = function() {
+    const rgb = document.querySelector('#rgb-color');
+    rgb.innerText = generateRgbColor();
+};
