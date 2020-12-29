0b0c461b26e1a8557f0a2d1b14c6f899f37e31a5
Felipe Rocha <felipe.sr27@gmail.com>
2020-12-29 14:40:02 -0300

    Requirement 3 and 4: created functions that displays balls.

--- script.js
+++ script.js
@@ -13,20 +13,30 @@ function sortRgb() {
 
 // Create a element Section in HTML
 function createSection(container) {
-  const section = document.createElement('p');
+  const section = document.createElement('section');
   container.appendChild(section);
   return section;
 }
 
 // Load RGB code in HTML
-function loadRgbCode() {
-  const container = document.querySelector('.main-page');
+function loadRgbCode(container) {
   const section = createSection(container);
   section.id = 'rgb-color';
   section.innerText = `(${sortRgb()})`;
 }
 
+function loadBalls(container, amount) {
+  for(let index = 0; index < amount; index += 1){
+    const section = createSection(container);
+    section.className = 'ball';
+    section.style.backgroundColor = `rgb(${sortRgb()})`;
+  }
+}
+
 // Load functions and event listeners at the window load
 window.onload = function () {
-  loadRgbCode();
+  const container = document.querySelector('.main-page');
+  const amountBalls = 6;
+  loadRgbCode(container);
+  loadBalls(container, amountBalls)
 }
