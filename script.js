5c33eb88d9bcbaabe5b268b9ccd0448014dc1f7d
LucioOSilva <lucioosilva00@gmail.com>
2020-12-29 15:16:48 -0300

    added reset button and refresh when click

--- script.js
+++ script.js
@@ -38,8 +38,51 @@ function chooseColor() {
 
 // função que verifica e valida o score
 
-createcircles(6);
+function verifiyRgbScore(elementOne, elementTwo) {
+  let score = document.querySelector('#score');
+  console.log(elementOne.style.backgroundColor.slice(3));
+  console.log(elementTwo.innerText);
+  if (elementOne.style.backgroundColor.slice(3) === elementTwo.innerText) {
+    const value = score.innerText;
+    let sum = Number(value) + 3;
+    score.innerText = sum;
+    resetGame();
+  } else {
+    resetGame();
+  }
+}
+
+function matchColors() {
+  const paragraph = document.querySelector('#rgb-color');
+  const sectionCircles = document.querySelector('#circles-section');
+  for (let index = 0; index < sectionCircles.childElementCount; index += 1) {
+    sectionCircles.children[index].addEventListener('click', function () {
+    verifiyRgbScore(sectionCircles.children[index], paragraph);
+    });
+  }
+  
+}
+
+// função adiciona reset game ao botão "resetar cores"
+
+function aplyResetbuttons() {
+  const button = document.querySelector('#reset');
+  button.addEventListener('click', function() {
+    resetGame();
+  });
+}
+
+// função reseta as cores e o valor a ser descoberto
+
+function resetGame() {
+  paintCircles();
+  chooseColor();
+}
+
+// funcões de inicialização
+
+createcircles(2);
 paintCircles();
 chooseColor();
-
-
+matchColors();
+aplyResetbuttons()
