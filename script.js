6e9df0c6cd0b4a8cea3d9b081985d6ebc35290ef
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-31 12:00:56 -0400

    Adiciona requisito 5

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cor = document.querySelector('#rgb-color');
 const ballsContainer = document.querySelector('#ballsContainer');
+const answer = document.querySelector('#answer');
 
 function randomColor() {
   const color = `(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
@@ -14,17 +15,31 @@ function createBalls() {
   for (let index = 0; index < 6; index += 1) {
     const ball = document.createElement('div');
     ball.className = 'ball';
+    ball.addEventListener('click', checkColor);
     ballsContainer.appendChild(ball);
   }
 }
 
 function randomizeBalls() {
   const balls = document.querySelectorAll('.ball');
+  const posRight = Math.floor(Math.random() * 6);
   for (let index = 0; index < balls.length; index += 1) {
-    balls[index].style.backgroundColor = 'rgb' + randomColor();
+    balls[index].style.backgroundColor = `rgb${randomColor()}`;
+  }
+  balls[posRight].style.backgroundColor = `rgb${cor.innerText}`;
+}
+
+function checkColor(event) {
+  if (event) {
+    if (event.target.style.backgroundColor === `rgb${cor.innerText}`) {
+      answer.innerText = 'Acertou!'
+    } else {
+      answer.innerText = 'Errou! Tente novamente!'
+    }
   }
 }
 
 rgbGuess();
 createBalls();
 randomizeBalls();
+checkColor();
