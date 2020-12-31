e2babd58404145019f896336f11bd0f2f84a2186
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-31 11:30:22 -0400

    Adiciona requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const cor = document.querySelector('#rgb-color');
+const ballsContainer = document.querySelector('#ballsContainer');
 
 function randomColor() {
-  let color = `(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+  const color = `(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
   return color;
 }
 
 function rgbGuess() {
   cor.innerText = randomColor();
 }
-  
+
+function createBalls() {
+  for (let index = 0; index < 6; index += 1) {
+    const ball = document.createElement('div');
+    ball.className = 'balls';
+    ballsContainer.appendChild(ball);
+  }
+}
+
+function randomizeBalls() {
+  const balls = document.querySelectorAll('.balls');
+  for (let index = 0; index < balls.length; index += 1) {
+    balls[index].style.backgroundColor = 'rgb' + randomColor();
+  }
+}
+
 rgbGuess();
+createBalls();
+randomizeBalls();
