a5282ee09c6976cc6b99205116b5e798c4a92149
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-31 11:36:59 -0400

    Corrige resuisitos 3 e 4

--- script.js
+++ script.js
@@ -13,13 +13,13 @@ function rgbGuess() {
 function createBalls() {
   for (let index = 0; index < 6; index += 1) {
     const ball = document.createElement('div');
-    ball.className = 'balls';
+    ball.className = 'ball';
     ballsContainer.appendChild(ball);
   }
 }
 
 function randomizeBalls() {
-  const balls = document.querySelectorAll('.balls');
+  const balls = document.querySelectorAll('.ball');
   for (let index = 0; index < balls.length; index += 1) {
     balls[index].style.backgroundColor = 'rgb' + randomColor();
   }
