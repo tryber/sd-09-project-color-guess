53f80fd3fbd98af57907a960ab24ab1c117a8633
Kevin Fraga <kevinciolac@gmail.com>
2021-01-02 19:22:36 -0300

    :zero::five:

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-const colors = document.getElementById('colors');
+const rgbColor = document.getElementById('rgb-color');
 const balls = document.getElementsByClassName('ball');
+const colorGuess = document.getElementById('colors');
+const answer = document.getElementById('answer');
 
 function randomColor() {
   let rgb = 'rgb(';
   const red = Math.ceil(Math.random() * 255);
   const green = Math.ceil(Math.random() * 255);
   const blue = Math.ceil(Math.random() * 255);
-  rgb += red + ', ' + green + ', ' + blue + ')';
+  rgb += `${red}, ${green}, ${blue})`;
   return rgb;
 }
 
 function colorFill() {
+  rgbColor.innerText = randomColor().split('rgb')[1];
   for (let index = 0; index < balls.length; index += 1) {
     balls[index].style.backgroundColor = randomColor();
   }
+  balls[Math.ceil((Math.random() * balls.length)) - 1].style.backgroundColor = `rgb${rgbColor.innerText}`;
 }
 
 colorFill();
+
+function checkAnswer(event) {
+  if (event.target.style.backgroundColor === `rgb${rgbColor.innerText}`) {
+    answer.innerText = 'Acertou!';
+  } else {
+    answer.innerText = 'Errou! Tente novamente!';
+  }
+}
+
+colorGuess.addEventListener('click', checkAnswer);
