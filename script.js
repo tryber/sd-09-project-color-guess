147bd5ab74fbf3c5f7267e8d884a3ab1233c9fa4
Enio Nicael <enionicael@gmail.com>
2021-01-02 00:56:02 -0300

    Feature: mostra na pagina a cor em rgb para ser adivinhada.

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
+let colors = [];
+
 const randomColor = function () {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
-  const color = `rgb(${red} , ${green} , ${blue})`;
+  const color = `(${red} , ${green} , ${blue})`;
   return color;
 };
 
-
 const displayColors = function () {
   const colorsContainer = document.querySelector('.colors-container');
-  let colors = [];
   for (let index = 0; index < 6; index += 1) {
     const color = randomColor();
     colors.push(color);
     const divElement = document.createElement('div');
     divElement.className = 'ball';
-    divElement.style.backgroundColor = color;
+    divElement.style.backgroundColor = `rgb${color}`;
     colorsContainer.appendChild(divElement);
   }
-  console.log(colors);
+};
+
+const ramdomSelectColor = function () {
+  const index = Math.floor(Math.random() * 6);
+  return index;
+}
+
+const displayColorRGB = function () {
+  const colorRGB = document.getElementById('rgb-color');
+  const index = ramdomSelectColor();
+  colorRGB.innerText = colors[index];
 }
 
 window.onload = function () {
   displayColors();
+  displayColorRGB();
 }
