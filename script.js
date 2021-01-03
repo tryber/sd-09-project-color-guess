5999ccc6149d0a3a2c9cebd51c21e2a341d0fa11
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-01-03 16:08:53 -0300

    estilizando projeto

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ const answer = document.querySelector('#answer');
 let score = 0;
 
 function randomColor() {
-  const red = Math.floor(Math.random() * 255);
-  const green = Math.floor(Math.random() * 255);
-  const blue = Math.floor(Math.random() * 255);
-  const rgbArray = [red, green, blue];
-  const color = `rgb(${rgbArray})`;
-  return color;
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 
 function checkAnswer(event) {
