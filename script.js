0d3dba4d1c16e69478f118dac7ee7323ec34270a
Leticia Lima <letylima96@gmail.com>
2021-01-04 01:44:57 -0300

    ajusta issues code climate

--- script.js
+++ script.js
@@ -4,47 +4,47 @@ const rgbText = document.querySelector('#rgb-color');
 const button = document.querySelector('#reset-game');
 
 function coloringCircles() {
-    for (let index = 0; index < ball.length; index += 1) {
-        let rNumber = Math.floor(Math.random() * 256);
-        let gNumber = Math.floor(Math.random() * 256);
-        let bNumber = Math.floor(Math.random() * 256);
-        ball[index].style.backgroundColor = `rgb(${rNumber}, ${gNumber}, ${bNumber})`;
-    }
+  for (let index = 0; index < ball.length; index += 1) {
+    let rNumber = Math.floor(Math.random() * 256);
+    let gNumber = Math.floor(Math.random() * 256);
+    let bNumber = Math.floor(Math.random() * 256);
+    ball[index].style.backgroundColor = `rgb(${rNumber}, ${gNumber}, ${bNumber})`;
+  }
 }
 coloringCircles();
 
 function scoreSum() {
-    const score = document.querySelector('#score');
-    let points = 0;
-    points += 3;
-    score.innerText = `Placar: ${points}`;
-    localStorage.setItem('score', points);
+  const score = document.querySelector('#score');
+  let points = 0;
+  points += 3;
+  score.innerText = `Placar: ${points}`;
+  localStorage.setItem('score', points);
 }
 
 function resetButton() {
-    let rNum = Math.floor(Math.random() * 256);
-    let gNum = Math.floor(Math.random() * 256);
-    let bNum = Math.floor(Math.random() * 256);
-    rgbText.innerText = `(${rNum}, ${gNum}, ${bNum})`;
-    localStorage.getItem('score');
+  let rNum = Math.floor(Math.random() * 256);
+  let gNum = Math.floor(Math.random() * 256);
+  let bNum = Math.floor(Math.random() * 256);
+  rgbText.innerText = `(${rNum}, ${gNum}, ${bNum})`;
+  localStorage.getItem('score');
 }
 resetButton();
 button.addEventListener('click', resetButton);
 
 function initialMessage() {
-    text.innerText = 'Escolha uma cor';
+  text.innerText = 'Escolha uma cor';
 }
 initialMessage();
 button.addEventListener('click', initialMessage);
 
 function messageAboutEachColor(event) {
-        if (event.target.style.backgroundColor !== rgbText.innerText) {
-            text.innerText = 'Errou! Tente novamente!';
-        } else {
-            text.innerText = 'Acertou!';
-            scoreSum();
-        }
+  if (event.target.style.backgroundColor !== rgbText.innerText) {
+    text.innerText = 'Errou! Tente novamente!';
+  } else {
+    text.innerText = 'Acertou!';
+    scoreSum();
+  }
 }
 for (let index = 0; index < ball.length; index += 1) {
-    ball[index].addEventListener('click', messageAboutEachColor);
+  ball[index].addEventListener('click', messageAboutEachColor);
 }
