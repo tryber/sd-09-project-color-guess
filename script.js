e3a6cda382ae4e9a0bb5764585d3dc7224565a6a
brenno-calado <brenno.calado@gmail.com>
2021-01-04 11:53:15 -0300

    Task 5 - Build answer when clicking

--- script.js
+++ script.js
@@ -30,12 +30,32 @@ function buildRandomCircleColors(index, colorToGuessPosition) {
 
 function buildCircles() {
   const circleAmount = 6;
-  const colorToGuessPosition = Math.floor(Math.random() * 6);
+  const colorToGuessPosition = Math.floor(Math.random() * circleAmount);
   for (let index = 0; index < circleAmount; index += 1) {
     const circle = document.createElement('div');
     circle.className = 'ball';
     circle.style.backgroundColor = `${buildRandomCircleColors(index, colorToGuessPosition)}`;
     document.body.appendChild(circle);
+    circle.addEventListener('click', findResult);
+  }
+}
+
+function buildAnswer() {
+  const answer = document.createElement('h2');
+  answer.id = 'answer';
+  answer.innerText = 'Escolha uma cor';
+  document.body.appendChild(answer);
+}
+
+function findResult(evt) {
+  const answer = document.querySelector('#answer');
+  const colorToGuess = document.querySelector('#rgb-color');
+  const prizeColor = `rgb${colorToGuess.innerText}`;
+  const clickedColor = evt.target.style.backgroundColor;
+  if (prizeColor === clickedColor) {
+    answer.innerText = 'Acertou!';
+  } else {
+    answer.innerText = 'Errou! Tente novamente!';
   }
 }
 
@@ -44,4 +64,5 @@ window.onload = function () {
   buildColorToGuess();
   buildCircles();
   buildRandomCircleColors();
+  buildAnswer();
 }
