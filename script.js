35368c5e236abcf9fd02e59fc67218d6c6c9b869
brenno-calado <brenno.calado@gmail.com>
2021-01-04 10:54:59 -0300

    Task 2 - build Color to Guess

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ function buildTitle() {
   document.body.appendChild(title);
 }
 
+function buildRandomColorNumbers() {
+const min = 0;
+const max = 255;
+return color = `(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+}
+
+function buildColorToGuess() {
+  const colorToGuess = document.createElement('p');
+  colorToGuess.id = 'rgb-color';
+  colorToGuess.innerText = `${buildRandomColorNumbers()}`;
+  document.body.appendChild(colorToGuess);
+}
+
 window.onload = function () {
   buildTitle();
+  buildColorToGuess();
 }
