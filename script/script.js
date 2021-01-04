3455c4ef1b6faf92556e5b2bab3cd662bef882a7
PeryclesF <perycles_reis@hotmail.com>
2021-01-04 12:35:53 -0300

    corrigindo bugs

--- script/script.js
+++ script/script.js
@@ -3,54 +3,53 @@ const textoCor = document.getElementById('rgb-color');
 const textoResposta = document.getElementById('answer');
 const botaoReiniciar = document.getElementById('reset-game');
 const placar = document.getElementById('score');
-var numero = 0;
+let numero = 0;
 const cores = [];
 
 function numeroAleatorio(mul) {
-    const numero = Math.floor(Math.random() * mul);
-    return numero;
+  const numero = Math.floor(Math.random() * mul);
+  return numero;
 };
 
 function corAleatoria() {
-    const rgb = `rgb(${numeroAleatorio(256)}, ${numeroAleatorio(256)}, ${numeroAleatorio(256)})`;
-    return rgb;
+  const rgb = `rgb(${numeroAleatorio(256)}, ${numeroAleatorio(256)}, ${numeroAleatorio(256)})`;
+  return rgb;
 }
 
 placar.textContent = window.sessionStorage.getItem('score') || 0;
 numero = parseInt(placar.textContent);
 
 function cliqueNaBola(event) {
-    const corSelecionada = event.target.style.backgroundColor;
-    if (corSelecionada === textoCor.textContent) {
-        textoResposta.textContent = "Acertou!";
-        placar.textContent = numero = numero + 3;
-        // aqui soma mais 3 para o placar...
-    } else {
-        textoResposta.textContent = "Errou! Tente novamente!";
-        if (numero === 0) {
-            numero = 1;
-        }
-        placar.textContent = numero = numero - 1;
+  const corSelecionada = event.target.style.backgroundColor;
+  if (corSelecionada === textoCor.textContent) {
+    textoResposta.textContent = "Acertou!";
+    placar.textContent = numero = numero + 3;
+    // aqui soma mais 3 para o placar...
+  } else {
+    textoResposta.textContent = "Errou! Tente novamente!";
+    if (numero === 0) {
+      numero = 1;
     }
-    window.sessionStorage.setItem('score', numero);
+    placar.textContent = numero = numero - 1;
+  }
+  window.sessionStorage.setItem('score', numero);
 }
 
-for (let bola = 0; bola < bolas.length; bola++) {
-    const cor = corAleatoria();
-    bolas[bola].style.backgroundColor = cor;
-    cores[bola] = cor;
-    botaoReiniciar.style.backgroundColor = corAleatoria();
-    bolas[bola].addEventListener('click', cliqueNaBola);
+for (let bola = 0; bola < bolas.length; bola += 1) {
+  const cor = corAleatoria();
+  bolas[bola].style.backgroundColor = cor;
+  cores[bola] = cor;
+  botaoReiniciar.style.backgroundColor = corAleatoria();
+  bolas[bola].addEventListener('click', cliqueNaBola);
 }
 
 function adicionarCorAleatoria(){
-    textoCor.textContent = cores[numeroAleatorio(6)]
+  textoCor.textContent = cores[numeroAleatorio(6)];
 }
-
 adicionarCorAleatoria();
 
 function reiniciaJogo() {
-    window.location.reload();
+  window.location.reload();
 }
 
 botaoReiniciar.addEventListener('click', reiniciaJogo);
