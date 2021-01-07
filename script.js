b2575d1c9ad491e39ab5c6319764c03389b5546b
cleber-teixeira <cleberlt@gmail.com>
2021-01-07 03:57:31 -0300

    Requisito 5 - Adiciona botão com a função comparar cores e retornar se está correto ou errado. :white_check_mark:

--- script.js
+++ script.js
@@ -13,13 +13,29 @@ function randomColor() {
 // Atribuir cor aos círculos
 function colorBall() {
   const balls = document.getElementsByClassName('ball');
-  for(let index = 0; index < balls.length; index += 1) {
+  for (let index = 0; index < balls.length; index += 1) {
     const color = randomColor();
     balls[index].style.backgroundColor = color;
   }
 }
 
+// Comparar a cor escolhida
+function compareColor() {
+  const circulos = document.getElementById('section');
+  const color1 = document.getElementById('rgb-color').innerText;
+  const text = document.getElementById('answer');
+  circulos.addEventListener('click', function (event) {
+    const color2 = event.target.style.backgroundColor;
+    if (color2 === color1) {
+      text.innerText = 'Acertou!';
+    } else {
+      text.innerText = 'Errou! Tente novamente!';
+    }
+  });
+}
+
 // Iniciar função após o carregamento da página
 window.onload = function () {
   colorBall();
-}
+  compareColor();
+};
