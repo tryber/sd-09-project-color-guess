bdb575c1c8b98c8910939644473729484ed91c2e
Perycles floriano <perycles_reis@hotamil.com>
2021-02-19 13:29:09 -0300

    ultimo requisito refatorando

--- script/script.js
+++ script/script.js
@@ -36,10 +36,10 @@ function cliqueNaBola(event) {
   } else {
     textoResposta.textContent = "Errou! Tente novamente!";
     caixa.style.border = `10px solid rgb(193 33 33)`;
-    if (numero === 0) {
-      numero = 1;
-    }
-    placar.textContent = numero = numero - 1;
+    // if (numero === 0) {
+    //   numero = 1;
+    // }
+    placar.textContent = numero -= 1;
   }
   window.sessionStorage.setItem('score', numero);
 }
